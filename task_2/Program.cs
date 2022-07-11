dfae1aa186802621de4fc1cef4ee407e51ccfede
Dara Kosova <darakosova.gb@gmail.com>
2022-07-11 23:21:57 +0400

    Выдаем количество цифр в числе

---
+++ task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5 */
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int number_of_digits = 0;
+
+for (int i = num; i != 0; i/=10)
+{
+    number_of_digits++;
+}
+
+Console.WriteLine(number_of_digits);
+
+/* 2 способ через логарифм  */
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine((int) Math.Log10(num) + 1); 
