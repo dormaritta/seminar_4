de97308b427b6d0fa942796e443179ee57f24bb5
Dara Kosova <darakosova.gb@gmail.com>
2022-07-12 00:31:46 +0400

    Факториал

---
+++ task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120 */
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int factorial = 1;
+
+for (int i = 2; i <= num; i++)
+{
+    factorial *= i;
+}
+
+Console.WriteLine(factorial);
